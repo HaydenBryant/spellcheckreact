850ffb5fda8656d3cf8baab67bf35c986803fd48
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-05-09 17:27:48 -0400

    testing fetch

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+
+app.get("/test", (req, res) => {
+    res.send(header: "<h1>Test is running</h1>")
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server is running on port: " + PORT))
